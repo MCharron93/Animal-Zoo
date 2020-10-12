6d84aed5312e0f806700657c337ee7f3fbe18a1b
Michelle Charron <michelle.charron93@gmail.com>
2020-10-12 16:08:10 -0600

    Played with feed button options

--- app/main.js
+++ app/main.js
@@ -13,28 +13,28 @@ import Zoo from "./models/zoo.js"
 
 
 let gator = new Alligator("Gator", "Green", "Male", 43, "210lbs", true)
-gator.eat('ham')
+// gator.eat('ham')
 
  let batsy = new Bat ("Batsy", "Black", "Female", 2, "8lbs", true)
-batsy.eat("fruit")
+// batsy.eat("fruit")
 
 let cappy = new Capybara("Cappy", "Brown", "Female", 28, "180lbs", true)
-cappy.eat("leaves")
+// cappy.eat("leaves")
 
 let diago = new Dingo("Diago", "Tan", "Male", 4, "50lbs", true)
-diago.eat("scraps")
+// diago.eat("scraps")
 
 let ellie = new Elephant("Ellie", "Grey", "Female", 66, "2500lbs", true)
-ellie.eat("leaves")
+// ellie.eat("leaves")
 
 let finn = new Frog("Finn", "Green", "Male", 1, "1lbs", false)
-finn.eat("flies")
+// finn.eat("flies")
 
 let greg = new Giraffe("Greg", "Yellow", "Male", 16, "800lbs", true)
-greg.eat("leaves")
+// greg.eat("leaves")
 
 let horris = new Hawk("Horris", "Multi", "Male", 3, "25lbs", true)
-horris.eat("mice")
+// horris.eat("mice")
 
 let animals = [batsy, cappy, ellie, finn, greg]
 let predators = [gator, diago, horris]
@@ -47,7 +47,7 @@ function drawAnimals(){
   let template = ""
 
   zoo.animals.forEach(animal => template += /*html*/ `
-  <div class="col-6">
+  <div class="col-4 card">
   <h1>${animal.name}</h1>
   <h4>Color: ${animal.color}</h4>
   <h4>Age: ${animal.age}</h4>
@@ -64,11 +64,12 @@ function drawPredators(){
   let template = ""
 
   zoo.predators.forEach(animal => template += /*html*/ `
-  <div class="col-4">
+  <div class="col-4 card">
   <h1>${animal.name}</h1>
   <h4>Color: ${animal.color}</h4>
   <h4>Age: ${animal.age}</h4>
   <h4>Weight: ${animal.weight}</h4>
+  <button id="food" onclick="feed()">Feed</button>
   </div>
   `)
 
